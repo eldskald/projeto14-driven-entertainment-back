0576e5f6498542be355be7afc3dc9178acc3f619
rafael <rafaelbordoni00@gmail.com>
2022-07-06 04:42:05 -0300

    Add validate token middleware

---
+++ src/middlewares/tokenValidation.js
@@ -0,0 +1,38 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import { ObjectId } from 'mongodb';
+import { db } from '../db.js';
+
+dotenv.config();
+const JWT_SECRET = process.env.JWT_SECRET;
+
+async function tokenValidation (req, res, next) {
+    try {
+        const { authorization } = req.headers;
+        if (authorization === '') {
+            res.locals.user = {};
+            next();
+        }
+
+        const token = authorization?.replace('Bearer ', '');
+        const { sessionId } = jwt.verify(token, JWT_SECRET);
+        const session = await db.sessions.findOne({ _id: ObjectId(sessionId) });
+        if (!session) {
+            return res.sendStatus(401);
+        }
+
+        const user = await db.accounts.findOne({ _id: session.userId });
+        if (!user) {
+            return res.sendStatus(401);
+        }
+
+        res.locals.user = user;
+        next();
+
+    } catch (err) {
+        console.log(err);
+        return res.sendStatus(401);
+    }
+}
+
+export default tokenValidation;
