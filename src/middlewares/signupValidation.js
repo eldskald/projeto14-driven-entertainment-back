b8b6fc1ef92afabb26c893af0e24e07d531b7fa4
rafael <rafaelbordoni00@gmail.com>
2022-07-07 02:55:13 -0300

    Remove sign up password confirmation from back-end

--- src/middlewares/signupValidation.js
+++ src/middlewares/signupValidation.js
@@ -5,7 +5,6 @@ const signupSchema = joi.object({
     name: joi.string().required().min(3),
     email: joi.string().email().required(),
     password:joi.string().pattern(/(?=.*?[A-Z])/).pattern(/(?=.*?[a-z])/).pattern(/(?=.*?[0-9])/).pattern(/(?=.*?[#?!@$%^&*-])/).min(8).required(),
-    passwordConfirm: joi.string().required().valid(joi.ref('password'))
 });
 
 async function signupValidation(req, res, next) {
@@ -38,30 +37,21 @@ async function signupValidation(req, res, next) {
                 if(err.includes('"password" is not allowed to be empty')){
                     return message+='Password field is not allowed to be empty!\n';
                 }
-                if((/\"passwordConfirm\" is required/).test(err)){
-                    return message+='Confirm your password!\n';
-                }
                 if((/\"email" must be a valid email/).test(err)){
                     return message+='Email must be a valid email!\n';
                 }
-                if((/"passwordConfirm" must be/).test(err)){
-                    return message+='Passwords must match!\n';
-                }
-                if(err.includes('"passwordConfirm" is not allowed to be empty')){
-                    return message+='Confirm password is not allowed to be empty!'
-                }
                 if(err.includes("/(?=.*?[A-Z])/")){
-                    return message+='Password must contains at least 1 capital character!\n';
+                    return message+='Password must contain at least 1 capital character!\n';
                 }
                 if(err.includes("/(?=.*?[a-z])/")){
-                    return message+='Password must contains at least 1 lower case character!\n';
+                    return message+='Password must contain at least 1 lower case character!\n';
                 }
                 if(err.includes("/(?=.*?[0-9])/")){
-                    return message+='Password must contains at least 1 number!\n';
+                    return message+='Password must contain at least 1 number!\n';
                 }if(err.includes('/(?=.*?[#?!@$%^&*-])/')){
-                    return message+='Password must contains at least 1 special character!\n';
+                    return message+='Password must contain at least 1 special character!\n';
                 }if(err.includes('"password" length must be at least 8 characters long')){
-                    return message+='Password must contains at least 8 characters long\n';
+                    return message+='Password must contain at least 8 characters long\n';
                 }else{
                     return message+=err+'\n';
                 }
@@ -72,10 +62,6 @@ async function signupValidation(req, res, next) {
         const searchEmail = await db.users.findOne({ email: body.email });
         if (searchEmail) {
             return res.status(409).send('Email already registered!');
-        }
-
-        if (body.password !== body.passwordConfirm) {
-            return res.status(401).send('Confirm the password!');
         }
 
         next();
