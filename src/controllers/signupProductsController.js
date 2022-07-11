8d23d347e2be6cb8989b808a9ee3d7207afffbc6
rafael <rafaelbordoni00@gmail.com>
2022-07-11 06:34:16 -0300

    Add create category and create subcategory endpoints

--- src/controllers/signupProductsController.js
+++ src/controllers/signupProductsController.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import { db } from "../db.js";
 
 export async function signupPoducts(req, res) {
@@ -20,4 +21,44 @@ export async function signupPoducts(req, res) {
     console.log("error to signup product at database", err);
     return res.sendStatus(500);
   }
+}
+
+// Corpo tem que ser no formato:
+// {
+//     category: <nome da categoria>
+// }
+//
+export async function createCategory(req, res) {
+    try {
+        const body = req.body;
+        await db.categories.insertOne({ category: body.category });
+        return res.sendStatus(201);
+    } catch(err) {
+        console.log(err);
+        return res.sendStatus(500);
+    }
+}
+
+// Corpo tem que ser no formato:
+// {
+//     subcategory: <nome da categoria>
+//     _idCategory: <id da categoria a qual pertence>
+// }
+//
+export async function createSubcategory(req, res) {
+    try {
+        const { subcategory, _idCategory } = req.body;
+
+        const catObj = await db.categories.findOne({ _id: ObjectId(_idCategory) })
+        if (!catObj) {
+            return res.sendStatus(404);
+        }
+
+        await db.subcategories.insertOne({ subcategory, _idCategory });
+        return res.sendStatus(201);
+        
+    } catch(err) {
+        console.log(err);
+        return res.sendStatus(500);
+    }
 }
--- src/routes/signupProductsRouter.js
+++ src/routes/signupProductsRouter.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { signupPoducts } from "../controllers/signupProductsController.js";
+import { signupPoducts, createCategory, createSubcategory } from "../controllers/signupProductsController.js";
 import { productValidation, verifyCategory, verifySubCategory } from "../middlewares/signupProductsValidation.js";
 
 const signupProductRouter = Router();
-
 signupProductRouter.post("/signup-products", productValidation, verifyCategory, verifySubCategory, signupPoducts);
-
+signupProductRouter.post("/new-category", createCategory);
+signupProductRouter.post("/new-subcategory", createSubcategory);
 export default signupProductRouter;
